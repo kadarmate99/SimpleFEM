703726eac53e119ed76419b55b5e565e80a45b3a
kadarmate99 <kadarmate99@gmail.com>
2025-11-17 09:34:05 +0100

    create basic DI setup with .NET Generic Host

--- SimpleFEM.UI/App.xaml.cs
+++ SimpleFEM.UI/App.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
 
 namespace SimpleFEM
@@ -9,6 +10,41 @@ namespace SimpleFEM
     /// </summary>
     public partial class App : Application
     {
-    }
 
+        private readonly IHost _host;
+
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.SetBasePath(AppContext.BaseDirectory)
+                    .AddJsonFile("appsettings.json", optional: false);
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSingleton<MainWindow>();
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await _host.StartAsync();
+
+            var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using (_host)
+            {
+                await _host.StopAsync();
+            }
+            base.OnExit(e);
+        }
+    }
 }
