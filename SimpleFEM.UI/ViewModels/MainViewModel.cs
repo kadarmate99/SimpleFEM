c39e90a025efcc021601119f6892b0acfc90dc12
kadarmate99 <kadarmate99@gmail.com>
2026-03-02 09:30:48 +0100

    feat: update window title upon model load

--- SimpleFEM.UI/ViewModels/MainViewModel.cs
+++ SimpleFEM.UI/ViewModels/MainViewModel.cs
@@ -1,18 +1,31 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using SimpleFEM.Data.Services;
 using SimpleFEM.UI.Navigation;
 
 namespace SimpleFEM.UI.ViewModels
 {
     public partial class MainViewModel : ObservableObject
     {
+        private readonly IModelFileService _modelFileService;
+
         [ObservableProperty]
         private string _windowTitle = "SimpleFEM";
 
         public INavigationService Navigation { get; }
 
-        public MainViewModel(INavigationService navigationService)
+        public MainViewModel(INavigationService navigationService, IModelFileService modelFileService)
         {
             Navigation = navigationService;
+            _modelFileService = modelFileService;
+
+            modelFileService.ModelFileChanged += OnModelFileChanged;
+        }
+
+        private void OnModelFileChanged(object? sender, EventArgs e)
+        {
+            WindowTitle = _modelFileService.IsModelFileOpen
+                ? $"SimpleFEM - {_modelFileService.CurrentModelFileName}"
+                : "SimpleFEM";
         }
     }
 }
